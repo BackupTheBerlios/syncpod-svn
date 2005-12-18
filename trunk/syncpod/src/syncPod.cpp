e958ed39969d2efb55f69497700afc84d6b4a9bf
jbl <jbl@2b5ab479-735d-0410-a86f-8670f50ec820>
2005-12-18 11:26:30 +0000

    Implement file logging, add ability to set the logging level

--- trunk/syncpod/src/syncPod.cpp
+++ trunk/syncpod/src/syncPod.cpp
@@ -5,6 +5,9 @@
 #include <dbt.h>
 #include <stdio.h>
 #include <getopt.h>
+#include <io.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #if defined HAVE_RLOG
 #include <rlog/rlog.h>
@@ -34,6 +37,10 @@ public:
    */  
   const char *configFile;
   /**
+   * Level of logging: 0: critical, 1: warnings, 2: everything
+   */
+  unsigned int logLevel;
+  /**
    * @brief Initialise the members to 0.
    */
   SyncPodOptions();
@@ -43,6 +50,7 @@ SyncPodOptions::SyncPodOptions()
 {
   logFile    = 0;
   configFile = 0;
+  logLevel   = 0;
 }
 
 static PodSync g_app;
@@ -97,14 +105,15 @@ static LRESULT CALLBACK WndProc( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPar
 static void usage( void )
 {
   printf( "Usage: syncPod [OPTIONS]\n" );
-  printf( "  -c --config FILE Use FILE as config (defaults to syncPod.cfg)\n" );
-  printf( "  -l --log FILE Log to FILE (requires rLog-enabled build: " );
+  printf( "  -c FILE Use FILE as config (defaults to syncPod.cfg)\n" );
+  printf( "  -l FILE Log to FILE (requires rLog-enabled build: " );
 #if defined HAVE_RLOG
   printf( "ON." );
 #else
   printf( "OFF." );
 #endif
   printf( ")\n" );
+  printf( "  -v LEVEL Set the logging level 0: critical(DEFAULT), 1: warnings, 2: everything.\n\n" );
   printf( "  -h --help Display this help and exit.\n\n" );
   printf( "Copyright (c)2005 Jean-Baptiste Lab (jean-baptiste dot lab at laposte dot net)\n" );
   printf( "See the file copying.txt for copying permission.\n" );
@@ -116,7 +125,7 @@ static bool parseOptions( int argc, char *argv[], SyncPodOptions *options )
   bool result = true;
   while (1)
   {  
-    c = getopt( argc, argv, "c:l:h" );
+    c = getopt( argc, argv, "c:l:v:h" );
     /* Detect the end of the options. */
     if( c == -1 )
       break;
@@ -127,6 +136,9 @@ static bool parseOptions( int argc, char *argv[], SyncPodOptions *options )
       break;
     case 'l':
       options->logFile = optarg;
+      break;
+    case 'v':
+      sscanf( optarg, "%u", &options->logLevel );
       break;
     case 'h':
       usage();
@@ -187,7 +199,8 @@ static bool createInvisibleWindow( void )
 int main( int argc, char *argv[] )
 {
 #if defined HAVE_RLOG
-  StdioNode log;
+  StdioNode *log = 0;
+  int fd = -1;
 #endif
   SyncPodOptions l_options;
   if( true == parseOptions( argc, argv, &l_options ))
@@ -196,9 +209,34 @@ int main( int argc, char *argv[] )
 #if defined HAVE_RLOG
     /** @todo Actually use the log file argument */
     if( 0 != l_options.logFile )
-    {      
+    {
+      fd = open( l_options.logFile, _O_CREAT | _O_TRUNC | _O_RDWR | _O_BINARY, _S_IWRITE | _S_IREAD );
+      if( -1 != fd )
+      {
+        log = new StdioNode( fd );
+      }
+      else
+      {
+        log = new StdioNode;
+      }
     }
-    log.subscribeTo( RLOG_CHANNEL("") );
+    else
+    {
+      log = new StdioNode;
+    }
+    switch( l_options.logLevel )
+    {
+    case 0:
+      log->subscribeTo( GetGlobalChannel("error") );
+      break;
+    case 1:
+      log->subscribeTo( GetGlobalChannel("error") );
+      log->subscribeTo( GetGlobalChannel("warning") );
+      break;
+    default:
+      log->subscribeTo( GetGlobalChannel("") );
+      break;
+    }
 #endif
     if( 0 != l_options.configFile )
     {
@@ -220,5 +258,12 @@ int main( int argc, char *argv[] )
       }
     }
   }
+#if defined HAVE_RLOG
+  if( -1 != fd )
+  {
+    close( fd );
+  }
+  delete log;
+#endif
   return 0;
 }
